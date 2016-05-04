56a69eb5e9ebee98d1eb8ca56de79e01d5ace73f
pomverte <vietnem@yahoo.fr>
2016-05-04 09:41:21 +0200

    merge test

--- src/test/java/fr/pomverte/CodeGenerationTest.java
+++ src/test/java/fr/pomverte/CodeGenerationTest.java
@@ -2,13 +2,21 @@ package fr.pomverte;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.junit.Test;
@@ -21,8 +29,7 @@ import example.avro.User;
  */
 public class CodeGenerationTest {
 
-    @Test
-    public void createSerializeDeserialize() throws IOException {
+    private List<User> createUser() {
         // CREATING USERS
         User user1 = new User();
         user1.setName("Alyssa");
@@ -35,15 +42,27 @@ public class CodeGenerationTest {
         // Construct via builder
         User user3 = User.newBuilder().setName("Charlie").setFavoriteColor("blue").setFavoriteNumber(null).build();
 
+        ArrayList<User> result = new ArrayList<User>();
+        result.add(user1);
+        result.add(user2);
+        result.add(user3);
+        return result;
+    }
+
+    @Test
+    public void fileSerializationTest() throws IOException {
+        List<User> users = this.createUser();
+
         // SERIALIZING
         // Serialize user1, user2 and user3 to disk
         DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
         DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
         File outputFile = new File("target/users.avro");
+        User user1 = users.get(0);
         dataFileWriter.create(user1.getSchema(), outputFile);
         dataFileWriter.append(user1);
-        dataFileWriter.append(user2);
-        dataFileWriter.append(user3);
+        dataFileWriter.append(users.get(1));
+        dataFileWriter.append(users.get(2));
         dataFileWriter.close();
 
         // DESERIALIZE USERS FROM DISK
@@ -63,4 +82,36 @@ public class CodeGenerationTest {
         assertEquals("3 Users have been created", 3, counter);
     }
 
+    @Test
+    public void streamSerializationTest() throws IOException {
+        List<User> users = this.createUser();
+
+        // SERIALIZING
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+        userDatumWriter.write(users.get(0), encoder);
+        userDatumWriter.write(users.get(1), encoder);
+        userDatumWriter.write(users.get(2), encoder);
+        // flush the binary code into the outputstream
+        encoder.flush();
+
+        // DESERIALIZE USERS FROM binary
+        DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+        BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
+
+        int counter = 0;
+        try {
+            while (true) {
+                User userDeserialized = new User();
+                userDatumReader.read(userDeserialized, decoder);
+                System.out.println(userDeserialized);
+                counter++;
+            }
+        } catch (EOFException e) {
+
+        }
+        assertEquals("3 Users have been created", 3, counter);
+    }
 }
