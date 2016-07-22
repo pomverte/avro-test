dd591ed80c77159f20823463827431bec5c2a98e
pomverte <vietnem@yahoo.fr>
2016-07-22 14:55:18 +0200

    Java7 try-with-resources Statement

--- src/main/java/fr/pomverte/WriterHelper.java
+++ src/main/java/fr/pomverte/WriterHelper.java
@@ -19,31 +19,26 @@ public class WriterHelper {
 
     public static <T extends SpecificRecordBase> File writeRecordToFile(Class<T> classz, List<T> records, File file) {
         DatumWriter<T> userDatumWriter = new SpecificDatumWriter<>(classz);
-        try {
-            DataFileWriter<T> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+        try (DataFileWriter<T> dataFileWriter = new DataFileWriter<>(userDatumWriter)) {
             File outputFile = new File("target/users.avro");
             T record1 = records.get(0);
             dataFileWriter.create(record1.getSchema(), outputFile);
             dataFileWriter.append(record1);
             dataFileWriter.append(records.get(1));
             dataFileWriter.append(records.get(2));
-            dataFileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return file;
     }
 
-    public static File writeGenericToFile(Schema schema, List<GenericRecord> records,
-            File outputFile) {
+    public static File writeGenericToFile(Schema schema, List<GenericRecord> records, File outputFile) {
         DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
-        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
-        try {
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter)) {
             dataFileWriter.create(schema, outputFile);
             for (GenericRecord record : records) {
                 dataFileWriter.append(record);
             }
-            dataFileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,17 +48,17 @@ public class WriterHelper {
     public static <T extends SpecificRecordBase> ByteArrayOutputStream writeRecordToStream(Class<T> classz,
             List<T> records) {
         DatumWriter<T> userDatumWriter = new SpecificDatumWriter<>(classz);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
             for (T record : records) {
                 userDatumWriter.write(record, encoder);
             }
             // flush the binary code into the outputstream
             encoder.flush();
+            return outputStream;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return outputStream;
+        return null;
     }
 }
